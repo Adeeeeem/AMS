d0d64b6e3a16d55d818c4f5f67599872a1014590
Mohamed Adem Ben Moussa <MohamedAdemBenMoussa@gmail.com>
2023-02-18 23:55:59 +0100

    Create Providers List

--- src/main/java/com/adem/ams/AmsApplication.java
+++ src/main/java/com/adem/ams/AmsApplication.java
@@ -1,15 +1,30 @@
 package com.adem.ams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.adem.ams.entities.Provider;
 
 @SpringBootApplication
 public class AmsApplication
 {
+	public static List <Provider> providers = new ArrayList <> ();
 
 	public static void main(String[] args)
 	{
+		Provider p1 = new Provider(1, "Nokia", "nokia@hotmail.fr", "China", "Vente Smart Phones");
+		Provider p2 = new Provider(2, "Samsung", "samsung@gmail.com", "Korea", "Vente Smart Phones et TV");
+		Provider p3 = new Provider(3, "HP", "hp@gmail.com", "USA", "Vente PC");
+		
+		providers.add(p1);
+		providers.add(p2);
+		providers.add(p3);
+
 		SpringApplication.run(AmsApplication.class, args);
+
 	}
 
 }
